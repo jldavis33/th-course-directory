6e46fbd27744cf1fd5925cf6753703fa05a0e1e2
Josh Davis <jldavis33@yahoo.com>
2020-10-21 22:07:59 -0500

    Appending `BrowserRouter` withing `App` - Adds navigation `Routes` - Adds links to navigation Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,10 @@ const Header = () => (
   <header>
     <span className="icn-logo"><i className="material-icons">code</i></span>
     <ul className="main-nav">
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Teachers</a></li>
-      <li><a href="#">Courses</a></li>
+      <li><a href="/">Home</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/teachers">Teachers</a></li>
+      <li><a href="/courses">Courses</a></li>
     </ul>    
   </header>
 );
